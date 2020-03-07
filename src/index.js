c4721fb44a695bfcf37fe5ad94bb6fcc43a16e33
sanal1989 <sanalsalynov@gmail.com>
2020-03-07 17:43:00 +0300

    feat: compelet task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrayExpr = expr.split("");
+    let arrayNumber = new Array();
+    let start=0;
+    for(let i=0; i<arrayExpr.length/10; i++){
+        let number=arrayExpr.slice(start,start+10);
+        arrayNumber.push(number.join(""));
+        start+=10;
+    }
+    start=0;
+    let morzeCode="";
+    let str="";
+    for(let i=0; i<arrayNumber.length; i++){
+        for(let j=0;j<5;j++){
+            let arraySign=arrayNumber[i].slice(start,start+2);
+            let sing = arraySign;
+            if(sing == "10"){
+                morzeCode+=".";
+            }else if(sing == "11"){
+                morzeCode+="-";
+            }else if(sing == "**"){
+                morzeCode+="**";
+            }
+            start+=2;
+        }
+        start=0;
+        if(morzeCode=="**********"){
+            str+=" ";
+        }
+        for (let key in MORSE_TABLE) {
+            if(morzeCode==key){
+                str+=MORSE_TABLE[key];
+            }
+        }
+        morzeCode="";
+    }
+    return str;
 }
 
 module.exports = {
